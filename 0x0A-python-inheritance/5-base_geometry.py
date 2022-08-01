35bacc12c03e035b72210049c32067f27f1d71ac
Dawitdkt <dawitkinfu5@gmail.com>
2022-08-01 23:53:50 +0300

    small fix

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # 5-base_geometry.py
-
 """Defines an empty class BaseGeometry."""
 
 
--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # 6-base_geometry.py
-
 """Defines a base geometry class BaseGeometry."""
 
 
